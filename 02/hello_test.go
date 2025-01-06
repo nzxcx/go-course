634b0effcefcd2d217cb3148c67adfc9dbc9ffd7
nzxcx <hugo.laclau@gmail.com>
2025-01-06 23:32:59 +0100

    first exercises

---
+++ 02/hello_test.go
@@ -0,0 +1,21 @@
+package hello
+
+import "testing"
+
+func TestSayHello(t *testing.T) {
+	subtests := []struct {
+		items  []string
+		result string
+	}{
+		{
+			result: "Hello, world!",
+		},
+	}
+
+	for _, st := range subtests {
+		if s := Say(st.items); s != st.result {
+			t.Errorf("wanted %s [%v], got %s", st.result, st.items, s)
+		}
+	}
+
+}
